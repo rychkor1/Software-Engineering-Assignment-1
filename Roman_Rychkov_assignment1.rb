a1860dc6bbf9e246d7cc11eee3e6984c8ddefec2
Student 1 Account <student1@hpc273.tcnj.edu>
2021-10-24 15:42:29 -0400

    revision 1

--- Roman_Rychkov_assignment1.rb
+++ Roman_Rychkov_assignment1.rb
@@ -6,57 +6,14 @@
 
 require 'csv' #ruby gem needed to read csv
 
-#Get the program ready to intake input from the user and the input files
-puts "Enter the number of FYS courses being offered: "
-num_courses = gets.to_i #chomp gets rid of the newline after the gets
-
-puts "Enter the number of students in the incoming class: "
-num_students = gets.to_i
-
-f = 1 #using a number to detect a file failure
-while f == 1 #switches to 0 if correct file to break from loop
-    puts "Enter the course list file name (include .csv at the end): "
-    input1 = gets.chomp
-    if File.file?(input1)
-        course_array = CSV.parse(File.read(input1), headers: true) #properly reads through the csv file and populates course_array
-        f = 0 #exits the loop as it found the correct file
-        break
-    else
-        puts "Wrong file name, try again \n"
-    end
-end
-
-f = 1 #using a number to detect a file failure
-while f == 1 #switches to 0 if correct file to break from loop
-    puts "Enter the student selections file name (include .csv at the end): "
-    input2 = gets.chomp
-    if File.file?(input2)
-        student_array = CSV.parse(File.read(input2), headers: true) #properly reads through the csv file and populates student_array
-        f = 0 #exits the loop as it found the correct file
-        break
-    else
-        puts "Wrong file name, try again \n"
-    end
-end
-
-#Ask the user to provide the names of the output files
-puts "Enter the name for output file 1 (include .txt at the end): "
-outfile1 = gets.chomp
-
-puts "Enter the name for output file 2 (include .txt at the end): "
-outfile2 = gets.chomp
-
-puts "Enter the name for output file 3 (include .txt at the end): "
-outfile3 = gets.chomp
-
 #Making a class for courses to fill in new objects with data from the input
 class Course
+    @@max_size = 18
     attr_accessor :course_id, :course_num, :course_title
     def initialize(course_id, course_num, course_title)
-        @course_id = course_id.to_s
-        @course_num = course_num.to_s
-        @course_title = course_title.to_s
-        @course_size = 0 #starts at 0 and will go up with every student added
+        @course_id = course_id
+        @course_num = course_num
+        @course_title = course_title
         @students = [] #keeps track of student ids in the course
     end
 
@@ -70,27 +27,104 @@ class Course
 
     def students_out #gets the string version of the array of student ids
         return (@students).join(", ")
-    end
+    end #iterate through array in ruby, for each, << to output
 
-    def add_student(id) #outputs the student id's array in the form of a string
-        @students << id.to_s 
+    def add_student(student_id) #outputs the student id's array in the form of a string
+        if course_size < max_size
+            @students << student_id
+            return true
+        return false
     end
     
 end
 
 #Making a class for student to fill in new objects with data from the input
 class Student
-    attr_accessor :student_id, :choice1, :choice2, :choice3, :choice4, :choice5, :choice6
-    def initialize(student_id, choice1, choice2, choice3, choice4, choice5, choice6)
-        @student_id = student_id.to_s
-        @choice1 = choice1.to_s
-        @choice2 = choice2.to_s
-        @choice3 = choice3.to_s
-        @choice4 = choice4.to_s
-        @choice5 = choice5.to_s
-        @choice6 = choice6.to_s
+    attr_accessor :student_id
+    def initialize(student_id)
+        @student_id = student_id #leave as integers not convert to strings
+        @choices = [] #choice array
+        @course_id = nil
+    end
+
+    def add_choice(course_id)
+        @choices << course_id
     end
 end
+
+#Get the program ready to intake input from the user and the input files
+puts "Enter the number of FYS courses being offered: "
+num_courses = gets.to_i #chomp gets rid of the newline after the gets
+
+puts "Enter the number of students in the incoming class: "
+num_students = gets.to_i
+
+f = 1 #using a number to detect a file failure
+while f == 1 #switches to 0 if correct file to break from loop
+    puts "Enter the course list file name (include .csv at the end): "
+    input1 = gets.chomp
+    if File.file?(input1)
+        course_array = CSV.parse(File.read(input1), headers: true) #properly reads through the csv file and populates course_array
+        course_map = {}
+        for course in course_array
+            course_id = course[0]
+            course_map[course_id] = Course.new(course[0].to_i, course[1].to_s, course[2].to_s)
+        f = 0 #exits the loop as it found the correct file
+        break #not needed
+    else
+        puts "Wrong file name, try again \n"
+    end
+end
+
+f = 1 #using a number to detect a file failure
+while f == 1 #switches to 0 if correct file to break from loop
+    puts "Enter the student selections file name (include .csv at the end): "
+    input2 = gets.chomp
+    if File.file?(input2)
+        student_array = CSV.parse(File.read(input2), headers: true) #properly reads through the csv file and populates student_array
+        student_map = {}
+        for student in student_array
+            student_id = student[0]
+            if student_id in student_map
+                course_id = student[1]
+                if course_id in course_map
+                    student_map[student_id].add_choice(course_id) #write function for add_course    
+            else
+                student_map[student_id] = Student.new(student[0].to_i)
+            end
+        end
+        f = 0 #exits the loop as it found the correct file
+        break #not needed
+    else
+        puts "Wrong file name, try again \n"
+    end
+end
+
+for student_id in student_map
+    student = student_map[student_id]
+    for choice in student.choices
+        if choice in course_map
+            course = course_map[choice]
+            if course.add_student(student_id)
+                student.course_id = course.course_id
+                break #if successful, then break
+    if student.course_id = nil
+        for course_id in course_map
+            if course.add_student(student_id)
+                student.course_id = course.course_id
+                break #if successful, then break
+                
+
+
+#Ask the user to provide the names of the output files
+puts "Enter the name for output file 1 (include .txt at the end): "
+outfile1 = gets.chomp
+
+puts "Enter the name for output file 2 (include .txt at the end): "
+outfile2 = gets.chomp
+
+puts "Enter the name for output file 3 (include .txt at the end): "
+outfile3 = gets.chomp
 
 #Populating the course array with course objects
 pop_course_array = [] #the populated course array with the course objects
@@ -98,7 +132,7 @@ num_courses.times do |index|
     id = course_array[index]["Id"]
     num = course_array[index]["Number"]
     title = course_array[index]["Title"]
-    pop_course_array[index] = Course.new(id, num, title)
+    pop_course_array[index] = Course.new(id, num, title) #check what output is <<
 end
 
 #Populating the student array with student objects
